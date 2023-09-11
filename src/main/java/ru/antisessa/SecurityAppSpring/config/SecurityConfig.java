672abdfe4af3123c2a09c5554487dc7e36fb748c
Antisessa <118179929+Antisessa@users.noreply.github.com>
2023-09-11 15:31:55 +0300

    add bcrypt for users password

--- src/main/java/ru/antisessa/SecurityAppSpring/config/SecurityConfig.java
+++ src/main/java/ru/antisessa/SecurityAppSpring/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.antisessa.SecurityAppSpring.services.PersonDetailsService;
@@ -23,26 +24,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // Настраиваем сам Spring Security и авторизацию
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
+        //отключение подделки межстраничных запросов
+        http.csrf().disable();
+
+        //настройка прав доступа для не авторизированных пользователей
+        http.authorizeRequests()
                 .antMatchers("/auth/login", "/error", "/auth/registration").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage("/auth/login")
+                .anyRequest().authenticated();
+
+        //настройка custom страницы авторизации
+        http.formLogin().loginPage("/auth/login")
                 .loginProcessingUrl("/process_login")
                 .defaultSuccessUrl("/hello", true)
                 .failureUrl("/auth/login?error");
+
+        //настройка выхода из учетной записи
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/auth/login");
     }
 
     // Настраивает аутентификацию
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
+        auth.userDetailsService(personDetailsService)
+                .passwordEncoder(getPasswordEncoder());
     }
 
     //Настраиваем шифрование паролей
     @Bean
     public PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/ru/antisessa/SecurityAppSpring/util/PersonValidator.java
+++ src/main/java/ru/antisessa/SecurityAppSpring/util/PersonValidator.java
@@ -6,18 +6,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import ru.antisessa.SecurityAppSpring.models.Person;
-import ru.antisessa.SecurityAppSpring.services.PersonDetailsService;
 import ru.antisessa.SecurityAppSpring.services.PersonService;
 
 @Component
 public class PersonValidator implements Validator {
 
-    private final PersonDetailsService personDetailsService;
     private final PersonService personService;
 
     @Autowired
-    public PersonValidator(PersonDetailsService personDetailsService, PersonService personService) {
-        this.personDetailsService = personDetailsService;
+    public PersonValidator(PersonService personService) {
         this.personService = personService;
     }
 
@@ -32,13 +29,5 @@ public class PersonValidator implements Validator {
 
         if(personService.findByUsername(person.getUsername()).isPresent())
             errors.rejectValue("username", "", "Человек с таким именем пользователя уже существует");
-
-//        try {
-//            personDetailsService.loadUserByUsername(person.getUsername());
-//        } catch (UsernameNotFoundException ignored) {
-//            return; // все ок, пользователь не найден
-//        }
-//
-//        errors.rejectValue("username", "", "Человек с таким именем пользователя уже существует");
     }
 }
